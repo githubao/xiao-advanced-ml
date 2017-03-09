241ccde7bac366eb4b50b3eff34f57f45de4e382
BaoQiang <mailbaoqiang@gmail.com>
2017-03-09 22:03:43 +0800

    tensor board

--- advml/learntf/tutorial.py
+++ advml/learntf/tutorial.py
@@ -16,6 +16,7 @@
 
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def mat_mul():
@@ -68,8 +69,9 @@ def build_nn():
     noise = np.random.normal(0, 0.05, x_data.shape)
     y_data = np.square(x_data) - 0.5 + noise
 
-    xs = tf.placeholder(tf.float32, [None, 1])
-    ys = tf.placeholder(tf.float32, [None, 1])
+    with tf.name_scope('inputs'):
+        xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+        ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
 
     # layer1 隐藏层 输入层维度*隐藏层维度
     l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
@@ -78,10 +80,16 @@ def build_nn():
     prediction = add_layer(l1, 10, 1, activation_function=None)
 
     # 损失
-    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+    with tf.name_scope('loss'):
+        loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 
     # 训练
-    train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
+    # 不同的学习优化器，对应于不同的学习效率
+    with tf.name_scope('train'):
+        train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
+        # train_step = tf.train.MomentumOptimizer(learning_rate=0.1).minimize(loss)
+        # train_step = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
+        # train_step = tf.train.RMSPropOptimizer(learning_rate=0.1).minimize(loss)
 
     # 初始化
     init = tf.global_variables_initializer()
@@ -89,26 +97,61 @@ def build_nn():
     with tf.Session() as sess:
         sess.run(init)
 
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        # 真实的数据
+        ax.scatter(x_data, y_data)
+        # 不让函数暂停挂起
+        plt.ion()
+        plt.show()
+
         for step in range(1000):
             result = sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
 
             if step % 50 == 0:
                 # print('step{}: {}'.format(step, result))
-                print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+                # print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+
+                try:
+                    ax.lines.remove(lines[0])
+                except Exception as e:
+                    pass
+
+                prediction_value = sess.run(prediction, feed_dict={xs: x_data, ys: y_data})
+                lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+
+                plt.pause(0.1)
+
+        writer = tf.summary.FileWriter('../logs/', sess.graph)
+        writer.add_graph(sess.graph)
+        writer.close()
+
+
+# 优化器
+def optimize():
+    pass
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
 
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
 
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
+        return outputs
 
-    return outputs
+
+def tf_board():
+    build_nn()
+    # tensorborad --logdir='logs/'
 
 
 def main():
@@ -116,7 +159,8 @@ def main():
     # variable()
     # place_hold()
     # active_func()
-    build_nn()
+    # build_nn()
+    tf_board()
 
 
 if __name__ == '__main__':
